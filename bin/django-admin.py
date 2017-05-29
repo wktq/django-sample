c21db651172f3d690533ac9662491ea593d02231
root <root@django-acabdc8c.c.bitnami-builder.internal>
2017-05-29 05:27:42 +0000

    initial commit

---
+++ bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/opt/bitnami/python/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==1.10.6','django-admin.py'
+__requires__ = 'Django==1.10.6'
+__import__('pkg_resources').run_script('Django==1.10.6', 'django-admin.py')
